5f46b3c877df3f1d9a3d1b293e193460570db60b
Kevin Horek <kevin@kevinhorek.com>
2013-01-05 15:30:33 -0700

    The Activity Lifecycle

--- src/com/kevinhorek/crystalball/MainActivity.java
+++ src/com/kevinhorek/crystalball/MainActivity.java
@@ -2,48 +2,59 @@ package com.kevinhorek.crystalball;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.kevinhorek.crystalball.ShakeDetector.OnShakeListener;
+
 public class MainActivity extends Activity {
-	
+
 	private CrystalBall mCrystalBall = new CrystalBall();
 	private TextView mAnswerLabel;
-	private Button mGetAnswerButton;
 	private ImageView mCrystalBallImage;
-
+	
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // Assigning the View variables and assign them the Views from the layout file
+        // Assign the Views from the layout file
         mAnswerLabel = (TextView) findViewById(R.id.textView1);
-        mGetAnswerButton = (Button) findViewById(R.id.button1);
-        mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
+    	mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
         
-        mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
-			
+    	mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    	mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    	mShakeDetector = new ShakeDetector(new OnShakeListener() {
+
 			@Override
-			public void onClick(View v) {	
-				
-				String answer = mCrystalBall.getAnAnswer();			
-				
-				//Update the label with our dynamic answer
-				mAnswerLabel.setText(answer);
-				
-				animateCrystalBall();
-				animateAnswer();
-				playSound();
+			public void onShake() {
+				handleNewAnswer();
 			}
 		});
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	mSensorManager.registerListener(mShakeDetector, mAccelerometer, 
+    			SensorManager.SENSOR_DELAY_UI);
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	mSensorManager.unregisterListener(mShakeDetector);
     }
     
     private void animateCrystalBall() {
@@ -82,5 +93,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	private void handleNewAnswer() {
+		String answer = mCrystalBall.getAnAnswer();			
+		
+		//Update the label with our dynamic answer
+		mAnswerLabel.setText(answer);
+		
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
+	}
     
 }
