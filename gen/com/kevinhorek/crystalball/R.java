e944d89cd57e5ee60d6b7715ef2c673186132fa6
Kevin Horek <kevin@kevinhorek.com>
2013-01-05 13:05:43 -0700

    Adding in image, theming and Customizing Buttons and Labels

--- gen/com/kevinhorek/crystalball/R.java
+++ gen/com/kevinhorek/crystalball/R.java
@@ -11,12 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ball01=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
-        public static final int textView1=0x7f070000;
+        public static final int button1=0x7f070002;
+        public static final int imageView1=0x7f070000;
+        public static final int menu_settings=0x7f070003;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
