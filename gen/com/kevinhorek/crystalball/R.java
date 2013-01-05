de70056886e24da90cfdee744d803a6324916a21
Kevin Horek <kevin@kevinhorek.com>
2013-01-05 14:09:17 -0700

    Adding Sounds

--- gen/com/kevinhorek/crystalball/R.java
+++ gen/com/kevinhorek/crystalball/R.java
@@ -39,23 +39,26 @@ public final class R {
         public static final int ic_launcher=0x7f020019;
     }
     public static final class id {
-        public static final int View2=0x7f070004;
-        public static final int button1=0x7f070001;
-        public static final int imageView1=0x7f070000;
-        public static final int menu_settings=0x7f070005;
-        public static final int textView1=0x7f070003;
-        public static final int view1=0x7f070002;
+        public static final int View2=0x7f080004;
+        public static final int button1=0x7f080001;
+        public static final int imageView1=0x7f080000;
+        public static final int menu_settings=0x7f080005;
+        public static final int textView1=0x7f080003;
+        public static final int view1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
+    }
+    public static final class raw {
+        public static final int crystal_ball=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -79,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/kevinhorek/crystalball/MainActivity.java
+++ src/com/kevinhorek/crystalball/MainActivity.java
@@ -2,6 +2,8 @@ package com.kevinhorek.crystalball;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -39,6 +41,7 @@ public class MainActivity extends Activity {
 				
 				animateCrystalBall();
 				animateAnswer();
+				playSound();
 			}
 		});
     }
@@ -58,6 +61,19 @@ public class MainActivity extends Activity {
     	fadeInAnimation.setFillAfter(true);
     	
     	mAnswerLabel.setAnimation(fadeInAnimation);
+    }
+    
+    private void playSound() {
+    	MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+    	player.start();
+    	player.setOnCompletionListener(new OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+				
+			}
+		});
     }
 
     @Override
