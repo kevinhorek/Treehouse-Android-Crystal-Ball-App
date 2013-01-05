fb15e99c0f220e15cf219ef71db024d35b753229
Kevin Horek <kevin@kevinhorek.com>
2013-01-05 13:17:57 -0700

    Positioning Text Using a Linear Layout

--- gen/com/kevinhorek/crystalball/R.java
+++ gen/com/kevinhorek/crystalball/R.java
@@ -15,10 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070002;
+        public static final int View2=0x7f070004;
+        public static final int button1=0x7f070001;
         public static final int imageView1=0x7f070000;
-        public static final int menu_settings=0x7f070003;
-        public static final int textView1=0x7f070001;
+        public static final int menu_settings=0x7f070005;
+        public static final int textView1=0x7f070003;
+        public static final int view1=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
