a5876f0e395e01a341e0252b9a0010d4fa327cb8
Kevin Horek <kevin@kevinhorek.com>
2013-01-05 13:49:19 -0700

    Animating the Answer

--- src/com/kevinhorek/crystalball/MainActivity.java
+++ src/com/kevinhorek/crystalball/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,17 +13,21 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	
 	private CrystalBall mCrystalBall = new CrystalBall();
+	private TextView mAnswerLabel;
+	private Button mGetAnswerButton;
+	private ImageView mCrystalBallImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // Declare our View variables and assign them the Views from the layout file
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-        Button getAnswerButton = (Button) findViewById(R.id.button1);
+        // Assigning the View variables and assign them the Views from the layout file
+        mAnswerLabel = (TextView) findViewById(R.id.textView1);
+        mGetAnswerButton = (Button) findViewById(R.id.button1);
+        mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
         
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+        mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {	
@@ -30,21 +35,29 @@ public class MainActivity extends Activity {
 				String answer = mCrystalBall.getAnAnswer();			
 				
 				//Update the label with our dynamic answer
-				answerLabel.setText(answer);
+				mAnswerLabel.setText(answer);
 				
 				animateCrystalBall();
+				animateAnswer();
 			}
 		});
     }
     
     private void animateCrystalBall() {
-    	ImageView crystalBallImage = (ImageView) findViewById(R.id.imageView1);
-    	crystalBallImage.setImageResource(R.drawable.ball_animation);
-    	AnimationDrawable ballAnimation = (AnimationDrawable) crystalBallImage.getDrawable();
+    	mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+    	AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
     	if (ballAnimation.isRunning()){
     		ballAnimation.stop();
     	}
     	ballAnimation.start();
+    }
+    
+    private void animateAnswer() {
+    	AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+    	fadeInAnimation.setDuration(1500);
+    	fadeInAnimation.setFillAfter(true);
+    	
+    	mAnswerLabel.setAnimation(fadeInAnimation);
     }
 
     @Override
